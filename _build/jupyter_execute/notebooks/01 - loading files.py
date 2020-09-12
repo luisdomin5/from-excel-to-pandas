804af926d8a37ac1498ac5a51615b2a6dc0389fe
Guy Ernest <guy.ernest@gmail.com>
2020-09-12 11:14:16 +0300

    adding simple examples for notebook 1

--- _build/jupyter_execute/notebooks/01 - loading files.py
+++ _build/jupyter_execute/notebooks/01 - loading files.py
@@ -1,8 +1,25 @@
 # Loading Files
 
+## Loading CSV file
+
 import pandas as pd
+import numpy as np
 
 table_df = pd.read_csv('../data/open_restaurant_applications_1.csv')
 
 table_df.head()
+
+table_df.info()
+
+table_df.describe()
+
+table_df.plot.scatter(x='longitude',y='latitude')
+
+## Adding new columns
+
+(
+    table_df
+    .assign(approved=lambda x : np.where(x.approved_for_roadway_seating == 'yes','g','r'))
+    .plot.scatter(x='longitude',y='latitude',c='approved')
+)
 
